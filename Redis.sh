3b75f6a57982a8dc403337bb647618333c13762b
sreecharan t <sreecharan.tumuluru@gmail.com>
2023-08-30 19:26:27 +0530

    added shell scripts for all modules added

---
+++ Redis.sh
@@ -0,0 +1,10 @@
+
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+yum install redis-6.2.11 -y
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf module enable redis:remi-6.2 -y
+yum install redis -y 
+
+systemctl enable redis
+systemctl start redis

7b333cfa315b2a6ff7fc8f76f8a85b5b38d6363a
sreecharan t <sreecharan.tumuluru@gmail.com>
2023-08-31 04:55:57 +0530

    removed flle content

--- Cart.sh
+++ Cart.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
-yum install nodejs -y
+# curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+# yum install nodejs -y
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
@@ -10,6 +10,7 @@ useradd roboshop
 
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
 cd /home/roboshop
+rm -rf cart
 unzip -o /tmp/cart.zip
 mv cart-main cart
 cd cart
--- Catalogue.sh
+++ Catalogue.sh
@@ -1,15 +1,17 @@
 #! /usr/bin/bash
 
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
-yum install nodejs -y
+# curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+# yum install nodejs -y
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+yum install nodejs -y 
 
 useradd roboshop
 
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 cd /home/roboshop
+
+rm -rf catalogue
 unzip -o /tmp/catalogue.zip
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
